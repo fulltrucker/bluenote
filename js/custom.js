9d2b71ce32b27a63586188c2ccf09f1612813132
Evan Chute <evan.chute@gmail.com>
2019-11-19 14:32:22 -0700

    tidy

--- js/custom.js
+++ js/custom.js
@@ -1,49 +1,50 @@
-$(document).ready(function(){
-	"use strict";
+$(document).ready(function() {
+    "use strict";
 
-  // Scroll To Top
+    // Scroll To Top
 
-		$(window).on('scroll',function () {
-			if ($(this).scrollTop() > 600) {
-				$('.return-to-top').fadeIn();
-			} else {
-				$('.return-to-top').fadeOut();
-			}
-		});
-		$('.return-to-top').on('click',function(){
-				$('html, body').animate({
-				scrollTop: 0
-			}, 1500);
-			return false;
-		});
-	
-	// Sticky Header
-		
-		$('#sticky-header').sticky({
-           topSpacing:0
-        });
-		
-	// Smooth Scrolling
-
-		$('a.smooth-menu').bind("click", function(event) {
-			event.preventDefault();
-			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top - 0
-			}, 1200,'easeInOutExpo');
-		});
-
-	// Scrollspy
-
-		$('body').scrollspy({
-			target:'.navbar-collapse',
-			offset:0
-		});
-
-	// Close mobile menu on click
-
-    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function () { 
-      $('.navbar-toggle:visible').click();
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 600) {
+            $('.return-to-top').fadeIn();
+        } else {
+            $('.return-to-top').fadeOut();
+        }
     });
 
-});	
+    $('.return-to-top').on('click', function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500);
+        return false;
+    });
+
+    // Sticky Header
+
+    $('#sticky-header').sticky({
+        topSpacing: 0
+    });
+
+    // Smooth Scrolling
+
+    $('a.smooth-menu').bind("click", function(event) {
+        event.preventDefault();
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top - 0
+        }, 1200, 'easeInOutExpo');
+    });
+
+    // Scrollspy
+
+    $('body').scrollspy({
+        target: '.navbar-collapse',
+        offset: 0
+    });
+
+    // Close mobile menu on click
+
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+});
